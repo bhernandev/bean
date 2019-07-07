21599aff0c354bc41a64486ca30fa6af950daa35
bhernandev <brian@hernandez-cs.com>
2019-07-07 17:52:16 -0400

    use proper import method

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask import Blueprint, render_template
 app = Flask(__name__, static_url_path='/static')
 
-from views import bean
+from .views import bean
 
 app.register_blueprint(bean)
